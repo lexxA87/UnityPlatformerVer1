c78e393bfb0fae84c7cbc7a4f52429f512f2d1ca
Alex Nikonov <60483531+lexxA87@users.noreply.github.com>
2023-04-21 23:01:14 +0400

    4. Animations
    Added animation for idle, walk and jump and refactored script for this

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody2D), typeof(Animator))]
 public class PlayerMove : MonoBehaviour
 {
+    [Header("Params")]
     [Range(1f, 50f)]
     [SerializeField]
     private float _speed = 5;
@@ -11,6 +12,12 @@ public class PlayerMove : MonoBehaviour
     [SerializeField]
     private float _jumpPower = 5;
 
+    [Header("Animation")]
+    [SerializeField] private string _jumpTriggerName = "JumpTrigger";
+    //[SerializeField] private string _attackTriggerName = "AttackTrigger";
+    [SerializeField] private string _isWalkName = "IsWalk";
+    //[SerializeField] private bool _isMoveBool = false;
+
     private bool _onGround = false;
     public bool OnGround
     {
@@ -18,26 +25,60 @@ public class PlayerMove : MonoBehaviour
     }
 
     private Rigidbody2D _body;
+    private Animator _animator;
+
+    private bool _isLeftMove = false;
 
     private void Awake()
     {
         _body = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
-    // Update is called once per frame
-    void Update()
+
+    private void CheckJump()
     {
-        var horizontalInput = Input.GetAxis("Horizontal");
-
-        if (horizontalInput != 0)
-        {
-            _body.velocity = new Vector2(horizontalInput * _speed, _body.velocity.y);
-        }
-
         if (Input.GetAxis("Jump") != 0 && _onGround)
         {
             _body.velocity = new Vector2(_body.velocity.x, _jumpPower);
             _onGround = false;
+            _animator.SetTrigger(_jumpTriggerName);
         }
+    }
+
+    private void CheckFlip(float horizontalInput)
+    {
+        var isLeftFlip = horizontalInput < -0.01f && !_isLeftMove;
+        var isRightFlip = horizontalInput > 0.01f && _isLeftMove;
+
+        if (isLeftFlip || isRightFlip)
+        {
+            _isLeftMove = !_isLeftMove;
+            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        }
+    }
+
+    private void CheckMove()
+    {
+        var horizontalInput = Input.GetAxis("Horizontal");
+
+        if (horizontalInput == 0)
+        {
+            _animator.SetBool(_isWalkName, false);
+            return;
+        }
+
+        _body.velocity = new Vector2(horizontalInput * _speed, _body.velocity.y);
+        _animator.SetBool(_isWalkName, true);
+
+        CheckFlip(horizontalInput);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        CheckMove();
+
+        CheckJump();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
