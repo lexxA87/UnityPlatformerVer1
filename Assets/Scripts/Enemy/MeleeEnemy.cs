557b4d121b37b32cf1ab4f206960c72d4c34a0c7
Alex Nikonov <60483531+lexxA87@users.noreply.github.com>
2023-05-07 23:16:48 +0400

    25. Enemy
    Added Skeleton with animations and new script of enemy, and attack of player

---
+++ Assets/Scripts/Enemy/MeleeEnemy.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+public class MeleeEnemy : MonoBehaviour
+{
+    [SerializeField] private float _attackCooldown;
+    [SerializeField] private float _range;
+    [SerializeField] private float _colliderDistance;
+    [SerializeField] private int _damage;
+    [SerializeField] private BoxCollider2D _boxCollider;
+    [SerializeField] private LayerMask _playerLayer;
+    private float _cooldownTimer = Mathf.Infinity;
+
+    private Animator _animator;
+    private PlayerHealth _playerHealth;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        _cooldownTimer += Time.deltaTime;
+
+        if (PlayerInSight())
+        {
+            if (_cooldownTimer > _attackCooldown)
+            {
+                //attack!
+                _cooldownTimer = 0;
+                _animator.SetTrigger("attack");
+            }
+        }
+    }
+
+    private bool PlayerInSight()
+    {
+        RaycastHit2D hit =
+            Physics2D.BoxCast(_boxCollider.bounds.center + transform.right * _range * transform.localScale.x * _colliderDistance,
+            new Vector3(_boxCollider.bounds.size.x * _range, _boxCollider.bounds.size.y, _boxCollider.bounds.size.z),
+            0, Vector2.left, 0, _playerLayer);
+
+        if (hit.collider != null)
+        {
+            _playerHealth = hit.transform.GetComponent<PlayerHealth>();
+        }
+
+        return hit.collider != null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawWireCube(_boxCollider.bounds.center + transform.right * _range * transform.localScale.x * _colliderDistance,
+            new Vector3(_boxCollider.bounds.size.x * _range, _boxCollider.bounds.size.y, _boxCollider.bounds.size.z));
+    }
+
+    private void DamagePlayer()
+    {
+        if (PlayerInSight())
+        {
+            _playerHealth.DamageHealth(_damage);
+        }
+    }
+}
