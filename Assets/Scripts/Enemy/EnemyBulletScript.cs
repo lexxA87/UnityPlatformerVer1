b76b05e72f6261e70063d7cdd67f3d9e1fca5c65
Alex Nikonov <60483531+lexxA87@users.noreply.github.com>
2023-05-16 16:58:52 +0400

    37. Idol_Boss
    Added FireBall for boss

--- Assets/Scripts/Enemy/EnemyBulletScript.cs
+++ Assets/Scripts/Enemy/EnemyBulletScript.cs
@@ -4,16 +4,21 @@ public class EnemyBulletScript : MonoBehaviour
 {
 
     private GameObject _player;
+    private EnemyCast _enemy;
     private Rigidbody2D _body2D;
-
-    [Range(0.1f, 10f)]
-    [SerializeField] private float _force;
+    private Animator _animator;
+    private int _damage;
+    private float _force;
 
     // Start is called before the first frame update
     void Start()
     {
         _body2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
         _player = GameObject.FindGameObjectWithTag("Player");
+        _enemy = GameObject.Find("HeadBoss").GetComponent<EnemyCast>();
+        _damage = _enemy.Damage;
+        _force = _enemy.Force;
 
         Vector3 direction = _player.transform.position - transform.position;
         _body2D.velocity = new Vector2(direction.x, direction.y).normalized * _force;
@@ -26,5 +31,23 @@ public class EnemyBulletScript : MonoBehaviour
     void Update()
     {
 
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "HeadBoss") return;
+
+        _body2D.velocity = new Vector2(0, 0);
+        _animator.SetTrigger("Explosion");
+
+        if (collision.CompareTag("Player"))
+        {
+            collision.GetComponent<PlayerHealth>().DamageHealth(_damage);
+        }
+    }
+
+    public void DestroyEnemyFireball()
+    {
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/Enemy/EnemyCast.cs
+++ Assets/Scripts/Enemy/EnemyCast.cs
@@ -7,17 +7,38 @@ public class EnemyCast : MonoBehaviour
     [SerializeField] private Transform _castPoint;
     [Range(0.1f, 10f)]
     [SerializeField] private float _castCalldown = 1;
+    [Range(0, 500)]
+    [SerializeField] private int _damage = 50;
+    [Range(0.1f, 10f)]
+    [SerializeField] private float _force;
+    [Range(0.1f, 20f)]
+    [SerializeField] private float _distanceAttack = 7;
 
     private float _timer;
+    private GameObject _player;
+
+    public int Damage { get { return _damage; } }
+    public float Force { get { return _force; } }
+
+    private void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+    }
 
     private void Update()
     {
-        _timer += Time.deltaTime;
+        float distance = Vector2.Distance(transform.position, _player.transform.position);
+        // Debug.Log(distance);
 
-        if (_timer > _castCalldown)
+        if (distance < _distanceAttack)
         {
-            _timer = 0;
-            Shoot();
+            _timer += Time.deltaTime;
+
+            if (_timer > _castCalldown)
+            {
+                _timer = 0;
+                Shoot();
+            }
         }
     }
 
