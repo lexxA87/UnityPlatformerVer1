33358b0565eba1383db936a237d387ecf26770a2
Alex Nikonov <60483531+lexxA87@users.noreply.github.com>
2023-05-18 18:13:58 +0400

    43. Idol_Boss
    Added new End Game Controller with timer, off animations of player and text press any key for reload new scene

--- Assets/Scripts/EndGame.cs
+++ Assets/Scripts/EndGame.cs
@@ -1,38 +1,58 @@
-using System.Timers;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class EndGame : MonoBehaviour
 {
-    [SerializeField] private Animator _player;
-    [SerializeField] private TextMeshProUGUI _text;
-    [SerializeField] private float _timeToEnd = 5000;
+    [SerializeField] private GameObject _player;
+    private Behaviour[] _playerComponents;
+    private Animator _playerAnimator;
+    [SerializeField] private Health _boss;
+    [SerializeField] private TextMeshProUGUI[] _texts;
+    private bool _isBossDead;
+    private float _timer;
 
     [Header("Reload")]
+    [SerializeField] private float _timeToReload = 3;
     [SerializeField] private int _reloadToScene;
+
+    private void Start()
+    {
+        _playerAnimator = _player.GetComponent<Animator>();
+        _playerComponents = new Behaviour[3];
+        _playerComponents[0] = _player.GetComponent<PlayerMove>();
+        _playerComponents[1] = _player.GetComponent<PlayerAttack>();
+        _playerComponents[2] = _player.GetComponent<PlayerCast>();
+    }
+
+    private void Update()
+    {
+        _isBossDead = _boss.IsDead;
+        if (_isBossDead)
+        {
+            FinishGame();
+            _isBossDead = false;
+        }
+
+        if (_texts[0].isActiveAndEnabled)
+        {
+            _timer += Time.deltaTime;
+            if (_timer > _timeToReload)
+            {
+                if (Input.anyKey)
+                    SceneManager.LoadScene(_reloadToScene);
+            }
+        }
+    }
 
     public void FinishGame()
     {
-        _player.SetBool("Win", true);
-        _text.gameObject.SetActive(true);
+        _playerAnimator.SetBool("Win", true);
 
-        Debug.Log("finish...");
+        foreach (Behaviour component in _playerComponents)
+            component.enabled = false;
 
-        SceneManager.LoadScene(_reloadToScene);
-
-
-        //var timer = new Timer(_timeToEnd);
-
-        //timer.Elapsed += OnEventExecution;
-
-        //timer.AutoReset = false;
-
-        //timer.Start();
-    }
-
-    private void OnEventExecution(System.Object? sender, ElapsedEventArgs eventArgs)
-    {
-        SceneManager.LoadScene(_reloadToScene);
+        foreach (TextMeshProUGUI text in _texts)
+            text.gameObject.SetActive(true);
     }
 }
